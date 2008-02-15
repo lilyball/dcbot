e0503ebf0f02caaabef537a00060cf628bb6ed16
Kevin Ballard <kevin@sb.org>
2008-02-15 08:08:01 -0500

    Fix up runloop to support read/write/error events properly

--- runloop.rb
+++ runloop.rb
@@ -1,36 +1,49 @@
+RunLoopSource = Struct.new :fd, :events, :proc
+
 class RunLoop
   class << RunLoop
     @@mainloop = nil
     
     def defaultRunLoop
-      @@mainloop |= RunLoop.new
+      @@mainloop ||= RunLoop.new
     end
     
     alias_method :default, :defaultRunLoop
   end
   
   def initialize
-    @sources = {}
+    @sources = []
     @running = false
   end
   
-  def add(src, &block)
-    @sources[src] = block
+  def add(src, types, &block)
+    @sources << RunLoopSource.new(src, types, block)
   end
   
-  def remove(src)
-    @sources.delete src
+  def remove(src, types = nil)
+    if types.nil? then
+      @sources.delete_if { |rls| rls.fd == src }
+    else
+      @sources.select { |rls| rls.fd == src }.each do |rls|
+        types.each do |type|
+          rls.events.delete type
+        end
+      end
+      @sources.delete_if { |rls| rls.events.empty? }
+    end
   end
   
   def run
     @running = true
     while @running
-      read, _, error = IO.select(@sources.keys, [], @sources.keys)
-      read.each do |fd|
-        @sources[fd].call(:read)
-      end
-      error.each do |fd|
-        @sources[fd].call(:error)
+      readers = @sources.select { |rls| rls.events.include? :read }.map { |rls| rls.fd }
+      writers = @sources.select { |rls| rls.events.include? :write }.map { |rls| rls.fd }
+      errorers = @sources.map { |rls| rls.fd }
+      read, write, error = IO.select(readers, writers, errorers)
+      [[read, :read], [write, :write], [error, :error]].each do |ary, event|
+        ary.each do |fd|
+          @sources.select { |rls| rls.fd == fd }.each { |rls| rls.proc.call(fd, event) }
+        end
       end
     end
   end
