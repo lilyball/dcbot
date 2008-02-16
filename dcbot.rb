68a51fb8d3b28ab1e1450900eba304c65ab05e90
Kevin Ballard <kevin@sb.org>
2008-02-16 09:03:13 -0500

    Migrate dcbot.rb to using DCProtocol

--- dcbot.rb
+++ dcbot.rb
@@ -2,12 +2,13 @@
 
 require 'rubygems'
 require 'optparse'
+require 'eventmachine'
 require './config'
-require './dcppsocket'
+require './dcprotocol'
 require './plugin'
 require 'pp'
 
-SLEEP_TABLE = [1, 2, 5, 15, 30, 60, 120, 300, 600, 1200, 1800]
+SLEEP_TABLE = [1, 2, 5, 15, 30, 60, 120, 300]
 
 def main
   # parse args
@@ -28,70 +29,73 @@ def main
   host = connection["host"]
   port = connection["port"]
   nickname = connection["nickname"]
+  description = connection["description"]
   
   if connection.has_key? "prefix" then
     PluginBase.cmd_prefix = connection["prefix"]
   end
   
-  catch :quit do
-    sleepIdx = 0
-    while true
-      # if runConnection exits instead of throwing :quit
-      # then the connection was closed
-      # if we can't reconnect, increase our sleep time before reconnect attempts
-      if !runConnection(host, port, nickname) then
-        STDERR.puts "Connection refused"
-        sleepTime = SLEEP_TABLE[sleepIdx]
-        STDERR.puts "Sleeping for #{sleepTime} seconds"
-        sleep sleepTime
-        sleepIdx += 1 unless sleepIdx == SLEEP_TABLE.size - 1
-      else
-        sleepIdx = 0
-        STDERR.puts "Connection closed"
-      end
-    end
+  EventMachine::run do
+    setupConnection(host, port, nickname, description, 0)
   end
   STDERR.puts "Shutting Down"
 end
 
-def runConnection(host, port, nickname)
-  begin
-    STDERR.puts "Connecting to #{host}:#{port} as #{nickname}"
-    socket = DCPPSocket.new(host, port, nickname)
-  rescue StandardError
-    return false
-  end
-  
-  STDERR.puts "Connected"
-  
-  socket.registerMessageCallback do |sender, message, isprivate|
-    puts "<#{sender}> #{message}" if isprivate or sender == "*Dtella"
-    if message[0,1] == PluginBase::CMD_PREFIX then
-      cmd, args = message[1..-1].split(" ", 2)
-      args = "" if args.nil?
-      if cmd == "reload" and isprivate then
-        # special sekrit reload command
-        PluginBase.loadPlugins
-        socket.sendPrivateMessage(sender, "Plugins have been reloaded")
-      elsif cmd == "quit" and isprivate then
-        # super-sekrit quit command
-        throw :quit
-      elsif PluginBase.has_command?(cmd) then
-        begin
-          PluginBase.dispatch(socket, cmd, sender, isprivate, args)
-        rescue StandardError => e
-          socket.sendPrivateMessage(sender, "An error occurred executing your command: #{e.to_s}")
-          STDERR.puts "ERROR: #{e.to_s}"
-          PP.pp(e.backtrace, STDERR)
+def setupConnection(host, port, nickname, description, sleep)
+  socket = DCProtocol.connect(host, port, nickname, :description => description) do |c|
+    c.registerCallback :message do |socket, sender, message, isprivate, isaction|
+      if isprivate or sender == "*Dtella" then
+        if isaction then
+          puts "<*> #{sender} #{message}"
+        else
+          puts "<#{sender} #{message}"
         end
-      elsif isprivate then
-        socket.sendPrivateMessage(sender, "Unknown command: #{PluginBase::CMD_PREFIX}#{cmd}")
+      end
+      if message[0,1] == PluginBase::CMD_PREFIX then
+        cmd, args = message[1..-1].split(" ", 2)
+        args = "" if args.nil?
+        if cmd == "reload" and isprivate then
+          # special sekrit reload command
+          PluginBase.loadPlugins
+          socket.sendPrivateMessage(sender, "Plugins have been reloaded")
+        elsif cmd == "quit" and isprivate then
+          # super-sekrit quit command
+          socket.close
+        elsif PluginBase.has_command?(cmd) then
+          begin
+            PluginBase.dispatch(socket, cmd, sender, isprivate, args)
+          rescue StandardError => e
+            socket.sendPrivateMessage(sender, "An error occurred executing your command: #{e.to_s}")
+            STDERR.puts "ERROR: #{e.to_s}"
+            PP.pp(e.backtrace, STDERR)
+          end
+        elsif isprivate then
+          socket.sendPrivateMessage(sender, "Unknown command: #{PluginBase::CMD_PREFIX}#{cmd}")
+        end
+      end
+    end
+    c.registerCallback :unbind do |socket|
+      if c.quit then
+        # this is our only socket for the moment
+        EventMachine.stop_event_loop
+      else
+        EventMachine::add_timer(SLEEP_TABLE[sleep]) do
+          sleep += 1 unless sleep == SLEEP_TABLE.size - 1
+          setupConnection(host, port, nickname, description, sleep)
+        end
+      end
+    end
+    c.registerCallback :connected do |socket|
+      socket.registerCallback :unbind do |socket|
+        if c.quit then
+          # this is our only socket for the moment
+          EventMachine.stop_event_loop
+        else
+          setupConnection(host, port, nickname, description, 0)
+        end
       end
     end
   end
-  
-  socket.startRunLoop
-  return true
 end
 
 main
