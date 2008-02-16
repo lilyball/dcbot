0c8f25434fc83d5045d741763f0e4b3aabdbf676
Kevin Ballard <kevin@sb.org>
2008-02-16 13:39:32 -0500

    Add !about command

--- plugins/help.rb
+++ plugins/help.rb
@@ -15,4 +15,19 @@ class HelpPlugin < PluginBase
   def self.cmd_help_help
     [nil, "Displays this help"]
   end
+  
+  def self.cmd_about(socket, sender, isprivate, args)
+    about = <<EOF
+I am a Direct Connect bot written in Ruby.
+I was written by Kevin Ballard <kevin@sb.org>.
+My code is available at http://repo.or.cz/w/dcbot.git
+EOF
+    about.split("\n").each do |line|
+      socket.sendPrivateMessage(sender, line)
+    end
+  end
+  
+  def self.cmd_about_help
+    [nil, "Displays information about this bot"]
+  end
 end
